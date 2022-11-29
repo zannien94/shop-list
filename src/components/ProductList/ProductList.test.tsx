4625002ee07bbaff8c5bf577df61fe5f33cde897
Zaneta <zannien94@gmail.com>
2022-11-29 22:10:47 +0100

    test ProductList

---
+++ src/components/ProductList/ProductList.test.tsx
@@ -0,0 +1,45 @@
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
+const mockedFunction = jest.fn()
+
+describe('<ProdutList/>', () => {
+  it('should be empty', () => {
+    render(<ProductList productList={[]} onClick={mockedFunction} />)
+    const list = screen.getByRole('list')
+    expect(list).toBeInTheDocument()
+  })
+
+  it('renders apple and banana', () => {
+    render(
+      <ProductList
+        productList={[
+          { name: 'apple', inBasket: false },
+          { name: 'banana', inBasket: false },
+        ]}
+        onClick={mockedFunction}
+      />
+    )
+    const apple = screen.getByText('apple')
+    const banana = screen.getByText('banana')
+
+    expect(apple).toBeInTheDocument()
+    expect(banana).toBeInTheDocument()
+  })
+
+  it('product apple should be click', async () => {
+    render(
+      <ProductList
+        productList={[
+          { name: 'apple', inBasket: false },
+          { name: 'banana', inBasket: false },
+        ]}
+        onClick={mockedFunction}
+      />
+    )
+    const apple = screen.getByText('apple')
+    await userEvent.click(apple)
+
+    expect(mockedFunction).toHaveBeenCalledTimes(1)
+  })
+})

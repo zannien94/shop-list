6446f9e68c616a975a73f0a0f6dade514aed4c26
Zaneta <zannien94@gmail.com>
2022-11-29 20:32:16 +0100

    test ProductListItem

---
+++ src/components/ProductListItem/ProductListItem.test.tsx
@@ -0,0 +1,18 @@
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
+const mockedFunction = jest.fn()
+
+describe('ProdutListItem/>', () => {
+  it('renders click text', () => {
+    render(<ProductListItem text='click' onClick={mockedFunction} />)
+    const listItem = screen.getByRole('listitem')
+    expect(listItem).toHaveTextDocument('click')
+  })
+  it('should run onClick once', async () => {
+    render(<ProductListItem text='click' onClick={mockedFunction} />)
+    const listItem = screen.getByRole('listitem')
+    await userEvent.click(listItem)
+    expect(mockedFunction).toHaveBeenCalledTimes(1)
+  })
+})

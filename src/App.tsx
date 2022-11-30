752ac92fbb5c905a419a4d2064f3d321c64c4fe9
Zaneta <zannien94@gmail.com>
2022-11-30 19:05:45 +0100

    add logic to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,19 @@
-import ProductList from './components/ProductList'
 import ShopItem from './components/ShopItem'
+import { useShopList } from './hooks/useShopList'
 
 function App() {
+  const { productsToBuy, productsInBasket, onProductClick } = useShopList()
   return (
     <div>
       <h1>Shop List</h1>
       <ShopItem
-        productList={[]}
-        onClick={() => {
-          console.log('add to basket')
-        }}
+        productList={productsToBuy}
+        onClick={onProductClick}
         title='Fruit I need to buy'
       />
       <ShopItem
-        productList={[]}
-        onClick={() => {
-          console.log('remove from basket')
-        }}
+        productList={productsInBasket}
+        onClick={onProductClick}
         title='Fruit I have'
       />
     </div>
